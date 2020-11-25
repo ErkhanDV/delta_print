e0a001bf18debef4c66776a9bd79754f2375f8dc
kotosvin <erhan.denis@list.ru>
2020-11-25 19:58:48 +0500

    #6

--- src/js/main.js
+++ src/js/main.js
@@ -4,91 +4,89 @@
   	let formdt = new FormData(this);
   	
   	let data = Object.fromEntries(formdt.entries());
-  	document.getElementById("ooo").textContent = data.ooo;
-  	document.getElementById("gendir").textContent = data.gendir;
-  	document.getElementById("inn").textContent = data.inn;
-  	document.getElementById("do").textContent = data.do;
-  	document.getElementById("dm").textContent = data.dm;
-  	document.getElementById("dm_summa").textContent = data.dm_summa;
-  	document.getElementById("ap").textContent = data.ap;
-  	document.getElementById("dnr").textContent = data.dnr;
-  	document.getElementById("summa_mo").textContent = data.summa_mo;
-  	document.getElementById("dt").textContent = data.dt;
-  	document.getElementById("nt").textContent = data.nt;
-  	document.getElementById("gorod").textContent = data.gorod;
-  	document.getElementById("ulitsa").textContent = data.ulitsa;
-  	document.getElementById("dom").textContent = data.dom;
-  	document.getElementById("ofis").textContent = data.ofis;
+  	document.getElementsByClassName("ooo").textContent = data.ooo;
+  	document.getElementsByClassName("gendir").textContent = data.gendir;
+  	document.getElementsByClassName("inn").textContent = data.inn;
+  	document.getElementsByClassName("do").textContent = data.do;
+  	document.getElementsByClassName("dm").textContent = data.dm;
+  	document.getElementsByClassName("dm_summa").textContent = data.dm_summa;
+  	document.getElementsByClassName("ap").textContent = data.ap;
+  	document.getElementsByClassName("dnr").textContent = data.dnr;
+  	document.getElementsByClassName("summa_mo").textContent = data.summa_mo;
+  	document.getElementsByClassName("dt").textContent = data.dt;
+  	document.getElementsByClassName("nt").textContent = data.nt;
+  	document.getElementsByClassName("gorod").textContent = data.gorod;
+  	document.getElementsByClassName("ulitsa").textContent = data.ulitsa;
+  	document.getElementsByClassName("dom").textContent = data.dom;
+  	document.getElementsByClassName("ofis").textContent = data.ofis;
 		
 		
 		if (data.mts_1) {
-			document.getElementById("mts_1").textContent = "V";
+			document.getElementsByClassName("mts_1").textContent = "V";
 		}	else {
-			document.getElementById("mts_1").textContent = " ";
+			document.getElementsByClassName("mts_1").textContent = " ";
 		}
 		if (data.os && data.ts) {
-			document.getElementById("os").textContent = "V";
-			document.getElementById("ts").textContent = "V";
-			document.getElementById("12mes").textContent = "V";
+			document.getElementsByClassName("os").textContent = "V";
+			document.getElementsByClassName("ts").textContent = "V";
+			document.getElementsByClassName("12mes").textContent = "V";
 		}	else if (data.os) {
-			document.getElementById("os").textContent = "V";
-			document.getElementById("9mes").textContent = "V";
+			document.getElementsByClassName("os").textContent = "V";
+			document.getElementsByClassName("9mes").textContent = "V";
 		} else if (data.ts) {
-			document.getElementById("ts").textContent = "V";
-			document.getElementById("6mes").textContent = "V";
+			document.getElementsByClassName("ts").textContent = "V";
+			document.getElementsByClassName("6mes").textContent = "V";
 		} else {
-			document.getElementById("os").textContent = " ";
-			document.getElementById("ts").textContent = " ";
-			document.getElementById("12mes").textContent = " ";
-			document.getElementById("9mes").textContent = " ";
-			document.getElementById("6mes").textContent = " ";
+			document.getElementsByClassName("os").textContent = " ";
+			document.getElementsByClassName("ts").textContent = " ";
+			document.getElementsByClassName("12mes").textContent = " ";
+			document.getElementsByClassName("9mes").textContent = " ";
+			document.getElementsByClassName("6mes").textContent = " ";
+		}
+		if (data.vkts) {
+			document.getElementsByClassName("vkts").textContent = "+ВКТС";
+		}	else {
+			document.getElementsByClassName("vkts").textContent = " ";
 		}
 		if (data.sms) {
-			document.getElementById("sms").textContent = "V";
+			document.getElementsByClassName("sms").textContent = "V";
 		}	else {
-			document.getElementById("sms").textContent = " ";
+			document.getElementsByClassName("sms").textContent = " ";
 		}
 		if (data.mts_2) {
-			document.getElementById("mts_2").textContent = "V";
+			document.getElementsByClassName("mts_2").textContent = "V";
 		}	else {
-			document.getElementById("mts_2").textContent = " ";
+			document.getElementsByClassName("mts_2").textContent = " ";
 		}
 		if (data.ps) {
-			document.getElementById("ps").textContent = "V";
+			document.getElementsByClassName("ps").textContent = "V";
 		}	else {
-			document.getElementById("ps").textContent = " ";
+			document.getElementsByClassName("ps").textContent = " ";
 		}
 		if (data[220]) {
-			document.getElementById("220").textContent = "V";
+			document.getElementsByClassName("220").textContent = "V";
 		}	else {
-			document.getElementById("220").textContent = " ";
+			document.getElementsByClassName("220").textContent = " ";
 		}
 		if (data.wl) {
-			document.getElementById("wl").textContent = "V";
+			document.getElementsByClassName("wl").textContent = "V";
 		}	else {
-			document.getElementById("wl").textContent = " ";
+			document.getElementsByClassName("wl").textContent = " ";
 		}
 		if (data.gl) {
-			document.getElementById("gl").textContent = "V";
+			document.getElementsByClassName("gl").textContent = "V";
 		}	else {
-			document.getElementById("gl").textContent = " ";
+			document.getElementsByClassName("gl").textContent = " ";
 		}
 		if (data.so) {
-			document.getElementById("so").textContent = "V";
+			document.getElementsByClassName("so").textContent = "V";
 		}	else {
-			document.getElementById("so").textContent = " ";
-		}
-		if ((data.ts) && (data.os)) {
-			document.getElementById("12mes").textContent = "V";
-		}	else {
-			document.getElementById("12mes").textContent = " ";
+			document.getElementsByClassName("so").textContent = " ";
 		}
 		window.print();
 
   }
 
 
-let form = document.getElementById('printer');
+var form = document.getElementById('printer');
 form.addEventListener('submit', for_print);	
-
-  
