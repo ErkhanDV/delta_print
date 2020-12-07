d072b0b2069feba603f5665bd7a6762afd7589bf
kotosvin <erhan.denis@list.ru>
2020-12-07 21:59:31 +0500

    #5

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,12 @@
   	let formdt = new FormData(this);
   	
   	let data = Object.fromEntries(formdt.entries());
-  	document.getElementById("ooo").textContent = data.ooo;
-  	document.getElementById("gendir").textContent = data.gendir;
-  	document.getElementById("inn").textContent = data.inn;
-  	document.getElementById("do").textContent = data.do;
-  	document.getElementById("dm").textContent = data.dm;
-  	document.getElementById("dm_summa").textContent = data.dm_summa;
   	document.getElementById("ap").textContent = data.ap;
   	document.getElementById("dnr").textContent = data.dnr;
-  	document.getElementById("summa_mo").textContent = data.summa_mo;
   	document.getElementById("dt").textContent = data.dt;
   	document.getElementById("nt").textContent = data.nt;
-  	document.getElementById("gorod").textContent = data.gorod;
-  	document.getElementById("ulitsa").textContent = data.ulitsa;
-  	document.getElementById("dom").textContent = data.dom;
-  	document.getElementById("ofis").textContent = data.ofis;
-		
-		
+  	document.getElementById("summa_mo").textContent = data.summa_mo;
+  	  	
 		if (data.mts_1) {
 			document.getElementById("mts_1").textContent = "V";
 		}	else {
@@ -29,19 +18,26 @@
 		if (data.os && data.ts) {
 			document.getElementById("os").textContent = "V";
 			document.getElementById("ts").textContent = "V";
-			document.getElementById("12mes").textContent = "V";
+			document.getElementById("mes12").textContent = "V";
 		}	else if (data.os) {
 			document.getElementById("os").textContent = "V";
-			document.getElementById("9mes").textContent = "V";
+			document.getElementById("mes9").textContent = "V";
 		} else if (data.ts) {
 			document.getElementById("ts").textContent = "V";
-			document.getElementById("6mes").textContent = "V";
+			document.getElementById("mes6").textContent = "V";
 		} else {
 			document.getElementById("os").textContent = " ";
 			document.getElementById("ts").textContent = " ";
-			document.getElementById("12mes").textContent = " ";
-			document.getElementById("9mes").textContent = " ";
-			document.getElementById("6mes").textContent = " ";
+			document.getElementById("mes12").textContent = " ";
+			document.getElementById("mes9").textContent = " ";
+			document.getElementById("mes6").textContent = " ";
+		}
+		if (data.ts && data.vkts) {
+			document.getElementById("vkts").textContent = "+ВКТС";
+		} else if (data.vkts) {
+			document.getElementById("vkts").textContent = "ВКТС";
+		} else {
+			document.getElementById("vkts").textContent = " ";
 		}
 		if (data.sms) {
 			document.getElementById("sms").textContent = "V";
@@ -58,10 +54,10 @@
 		}	else {
 			document.getElementById("ps").textContent = " ";
 		}
-		if (data[220]) {
-			document.getElementById("220").textContent = "V";
+		if (data.v220) {
+			document.getElementById("v220").textContent = "V";
 		}	else {
-			document.getElementById("220").textContent = " ";
+			document.getElementById("v220").textContent = " ";
 		}
 		if (data.wl) {
 			document.getElementById("wl").textContent = "V";
@@ -79,16 +75,46 @@
 			document.getElementById("so").textContent = " ";
 		}
 		if ((data.ts) && (data.os)) {
-			document.getElementById("12mes").textContent = "V";
+			document.getElementById("mes12").textContent = "V";
 		}	else {
-			document.getElementById("12mes").textContent = " ";
+			document.getElementById("mes12").textContent = " ";
 		}
+
+		for (let spn of document.getElementsByClassName("ooo")) {
+  	spn.textContent = data.ooo;
+  	}
+  	for (let spn of document.getElementsByClassName("gendir")) {
+  	spn.textContent = data.gendir;
+  	}
+		for (let spn of document.getElementsByClassName("inn")) {
+  	spn.textContent = data.inn;
+  	}
+  	for (let spn of document.getElementsByClassName("do")) {
+  	spn.textContent = data.do;
+  	}
+  	for (let spn of document.getElementsByClassName("dm")) {
+  	spn.textContent = data.dm;
+  	}
+  	for (let spn of document.getElementsByClassName("dm_summa")) {
+  	spn.textContent = data.dm_summa;
+  	}
+  	for (let spn of document.getElementsByClassName("gorod")) {
+  	spn.textContent = data.gorod;
+  	}
+  	for (let spn of document.getElementsByClassName("ulitsa")) {
+  	spn.textContent = data.ulitsa;
+  	}
+  	for (let spn of document.getElementsByClassName("dom")) {
+  	spn.textContent = data.dom;
+  	}
+  	for (let spn of document.getElementsByClassName("ofis")) {
+  	spn.textContent = data.ofis;
+  	}
+  	
 		window.print();
 
   }
 
-
 let form = document.getElementById('printer');
-form.addEventListener('submit', for_print);	
-
+form.addEventListener('submit', for_print);
   
