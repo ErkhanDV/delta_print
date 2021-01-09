6095f5a806d5b1c12f0a3143aacb29da44775ddd
kotosvin <erhan.denis@list.ru>
2021-01-10 01:31:55 +0500

    #7 ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,78 @@
+function date_today() {
+  var d = new Date();
+  var month = new Array ("января","февраля","марта","апреля","мая","июня",
+    "июля","августа","сентября","октября","ноября","декабря");
+  var data = (d.getDate() + " " +  month[d.getMonth()] + " " + d.getFullYear());
+  return data;
+}
+
 function for_print(evt) {
   evt.preventDefault();
-  	
+    
   let formdt = new FormData(this);
 
   let data = Object.fromEntries(formdt.entries());
-  let mass_1 = ["ap", "dnr", "dt", "nt", "summa_mo"];
+  let mass_1 = [ "ap", "dnr", "dt", "nt", "summa_mo" ];
   for (let item of mass_1) {
-  	document.getElementById(item).textContent = data[item];
-	}
-
-	let mass_2 = ["mts_1", "sms", "mts_2", "ps", "v220", "wl", "gl", "so" ];
-	for (let item of mass_2) {
-		if (data[item]) {
-			document.getElementById(item).textContent = "V";
-		}	else {
-			document.getElementById(item).textContent = " ";
-		}
-	}
-
-	let mass_3 = ["ooo", "gendir", "inn", "do", "dm", "dm_summa", "gorod", "ulitsa", "dom", "ofis"];
-	for (let item of mass_3) {
-		for (let spn of document.getElementsByClassName(item)) {
-  		spn.textContent = data[item];
-  	}
+    document.getElementById(item).textContent = data[item];
   }
 
-	if (data.os && data.ts) {
-		document.getElementById("os").textContent = "V";
-		document.getElementById("ts").textContent = "V";
-		document.getElementById("mes12").textContent = "V";
-	}	else if (data.os) {
-		document.getElementById("os").textContent = "V";
-		document.getElementById("mes9").textContent = "V";
-	} else if (data.ts) {
-		document.getElementById("ts").textContent = "V";
-		document.getElementById("mes6").textContent = "V";
-	} else {
-		document.getElementById("os").textContent = " ";
-		document.getElementById("ts").textContent = " ";
-		document.getElementById("mes12").textContent = " ";
-		document.getElementById("mes9").textContent = " ";
-		document.getElementById("mes6").textContent = " ";
-	}
-	if (data.ts && data.vkts) {
-		document.getElementById("vkts").textContent = "+ВКТС";
-	} else if (data.vkts) {
-		document.getElementById("vkts").textContent = "ВКТС";
-	} else {
-		document.getElementById("vkts").textContent = " ";
-	}
-	if ((data.ts) && (data.os)) {
-		document.getElementById("mes12").textContent = "V";
-	}	else {
-		document.getElementById("mes12").textContent = " ";
-	}
+  let mass_2 = [ "mts_1", "sms", "mts_2", "ps", "v220", "wl", "gl", "so" ];
+  for (let item of mass_2) {
+    if (data[item]) {
+      document.getElementById(item).textContent = "V";
+    } else {
+      document.getElementById(item).textContent = " ";
+    }
+  }
 
-  	
-	window.print();
+  let mass_3 = [ "ooo", "gendir", "inn", "do", "dm", "dm_summa", "gorod", "ulitsa", "dom", "ofis" ];
+  for (let item of mass_3) {
+    for (let spn of document.getElementsByClassName(item)) {
+      spn.textContent = data[item];
+    }
+  }
+  
+  let new_date = date_today();
+  for (let spn of document.getElementsByClassName("date_today")) {
+  spn.textContent = new_date;
+  }
+
+  if (data.os && data.ts) {
+    document.getElementById("os").textContent = "V";
+    document.getElementById("ts").textContent = "V";
+    document.getElementById("mes12").textContent = "V";
+  } else if (data.os) {
+    document.getElementById("os").textContent = "V";
+    document.getElementById("mes9").textContent = "V";
+  } else if (data.ts) {
+    document.getElementById("ts").textContent = "V";
+    document.getElementById("mes6").textContent = "V";
+  } else {
+    document.getElementById("os").textContent = " ";
+    document.getElementById("ts").textContent = " ";
+    document.getElementById("mes12").textContent = " ";
+    document.getElementById("mes9").textContent = " ";
+    document.getElementById("mes6").textContent = " ";
+  }
+  if (data.ts && data.vkts) {
+    document.getElementById("vkts").textContent = "+ВКТС";
+  } else if (data.vkts) {
+    document.getElementById("vkts").textContent = "ВКТС";
+  } else {
+    document.getElementById("vkts").textContent = " ";
+  }
+  if ((data.ts) && (data.os)) {
+    document.getElementById("mes12").textContent = "V";
+  } else {
+    document.getElementById("mes12").textContent = " ";
+  }
+
+    
+  window.print();
 
   }
 
 
 let form = document.getElementById('printer');
 form.addEventListener('submit', for_print);
-  
