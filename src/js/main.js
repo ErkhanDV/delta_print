9d899f3004426523aa17d52509ef962e9c743325
kotosvin <erhan.denis@list.ru>
2021-02-09 23:20:29 +0500

    #13

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
+var check_ul_fl = true;
 function date_today() {
   var d = new Date();
   var month = new Array ("января","февраля","марта","апреля","мая","июня",
     "июля","августа","сентября","октября","ноября","декабря");
   return `${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()}`;
 }
+
+function main_date() {
+  let mdate = document.getElementById("main_date");
+  mdate.value = date_today();
+}
+
+document.addEventListener("DOMContentLoaded", function()
+ {
+     main_date();
+ }, false);
 
 function for_print(evt) {
   evt.preventDefault();
@@ -25,118 +36,103 @@ function for_print(evt) {
     }
   }
 
-  let mass_3 = [ "ooo", "gendir", "inn", "do", "dm", "dm_summa", "gorod", "ulitsa", "dom", "ofis" ];
+  let mass_3 = [ "do", "dm", "dm_summa", "gorod", "ulitsa", "dom", "main_date", "ob_number" ];
   for (let item of mass_3) {
     for (let spn of document.getElementsByClassName(item)) {
       spn.textContent = data[item];
     }
   }
 
-  let new_date = date_today();
-    for (let spn of document.getElementsByClassName("date_today")) {
-      spn.textContent = new_date;
+  if (data.ts && data.vkts) {
+      document.getElementById("vkts").textContent = "+ВКТС";
+    } else if (data.vkts) {
+      document.getElementById("vkts").textContent = "ВКТС";
+    } else {
+      document.getElementById("vkts").textContent = " ";
+  }
+
+  if (check_ul_fl) {
+    let mass_4 = [ "ooo", "gendir", "inn", "ofis" ];
+    for (let item of mass_4) {
+      for (let spn of document.getElementsByClassName(item)) {
+        spn.textContent = data[item];
+      }
     }
 
-  if (data.os && data.ts) {
-    document.getElementById("os").textContent = "V";
-    document.getElementById("ts").textContent = "V";
-    document.getElementById("mes12").textContent = "V";
-  } else if (data.os) {
-    document.getElementById("os").textContent = "V";
-    document.getElementById("mes9").textContent = "V";
-  } else if (data.ts) {
-    document.getElementById("ts").textContent = "V";
-    document.getElementById("mes6").textContent = "V";
+    if (data.os && data.ts) {
+      document.getElementById("os").textContent = "V";
+      document.getElementById("ts").textContent = "V";
+      document.getElementById("mes12").textContent = "V";
+    } else if (data.os) {
+      document.getElementById("os").textContent = "V";
+      document.getElementById("mes9").textContent = "V";
+    } else if (data.ts) {
+      document.getElementById("ts").textContent = "V";
+      document.getElementById("mes6").textContent = "V";
+    } else {
+      document.getElementById("os").textContent = " ";
+      document.getElementById("ts").textContent = " ";
+      document.getElementById("mes12").textContent = " ";
+      document.getElementById("mes9").textContent = " ";
+      document.getElementById("mes6").textContent = " ";
+    }
+
+    if ((data.ts) && (data.os)) {
+      document.getElementById("mes12").textContent = "V";
+    } else {
+      document.getElementById("mes12").textContent = " ";
+    }
   } else {
-    document.getElementById("os").textContent = " ";
-    document.getElementById("ts").textContent = " ";
-    document.getElementById("mes12").textContent = " ";
-    document.getElementById("mes9").textContent = " ";
-    document.getElementById("mes6").textContent = " ";
-  }
-  if (data.ts && data.vkts) {
-    document.getElementById("vkts").textContent = "+ВКТС";
-  } else if (data.vkts) {
-    document.getElementById("vkts").textContent = "ВКТС";
-  } else {
-    document.getElementById("vkts").textContent = " ";
-  }
-  if ((data.ts) && (data.os)) {
-    document.getElementById("mes12").textContent = "V";
-  } else {
-    document.getElementById("mes12").textContent = " ";
-  }
+    let mass_5 = [ "os", "ts" ];
+      for (let item of mass_5) {
+        if (data[item]) {
+          document.getElementById(item).textContent = "V";
+        } else {
+          document.getElementById(item).textContent = " ";
+        }
+      }
+    let mass_6 = [ "fio", "bday", "pas_num", "pas_date", "pas_whoes", "pas_propis", "kvartira" ];
+      for (let item of mass_6) {
+        for (let spn of document.getElementsByClassName(item)) {
+        spn.textContent = data[item];
+        }
+      }
+  } 
 
   window.print();
-
-  }
+}
 
 let form = document.getElementById('printer');
 form.addEventListener('submit', for_print);
 
-function for_print_fl(evt) {
-  evt.preventDefault();
-    
-  let formdt = new FormData(this);
-
-  let data = Object.fromEntries(formdt.entries());
-  let mass_1 = [ "ap", "dnr", "dt", "nt", "summa_mo" ];
-  for (let item of mass_1) {
-    document.getElementById(item).textContent = data[item];
-  }
-
-  let mass_2 = [ "mts_1", "sms", "mts_2", "ps", "v220", "wl", "gl", "so", "os", "ts" ];
-  for (let item of mass_2) {
-    if (data[item]) {
-      document.getElementById(item).textContent = "V";
-    } else {
-      document.getElementById(item).textContent = " ";
-    }
-  }
-
-
-  let mass_3 = [ "fio", "bday", "pas_num", "pas_date", "pas_whoes", "pas_propis", "do", "dm", "dm_summa", "gorod", "ulitsa", "dom", "kvartira" ];
-  for (let item of mass_3) {
-    for (let spn of document.getElementsByClassName(item)) {
-      spn.textContent = data[item];
-    }
-  }
-
-  let new_date = date_today();
-    for (let spn of document.getElementsByClassName("date_today")) {
-      spn.textContent = new_date;
-    }
-
-  window.print();
-
-  }
-
-let form_fl = document.getElementById('printer_fl');
-form_fl.addEventListener('submit', for_print_fl);
-
 function ShowFL() {
-  document.getElementById("fiz_l_form").style.display="block"
-  document.getElementById("ur_l_form").style.display="none"
+  let fl_array = document.getElementsByClassName("only_fl");
+    for (let i = 0; i < fl_array.length; i ++) {
+      fl_array[i].style.display = "block";
+    }
+  let ul_array = document.getElementsByClassName("only_ul");
+    for (let i = 0; i < ul_array.length; i ++) {
+      ul_array[i].style.display = "none";
+    }
+  check_ul_fl = false;
 }
 function ShowUL() {
-  document.getElementById("fiz_l_form").style.display="none"
-  document.getElementById("ur_l_form").style.display="block"
+  let fl_array = document.getElementsByClassName("only_fl");
+    for (let i = 0; i < fl_array.length; i ++) {
+      fl_array[i].style.display = "none";
+    }
+  let ul_array = document.getElementsByClassName("only_ul");
+    for (let i = 0; i < ul_array.length; i ++) {
+      ul_array[i].style.display = "block";
+    }
+  check_ul_fl = true;
 }
 
 function check() {
   let checked_box = document.getElementById("inviz");
     if (checked_box.checked) {
-    document.getElementById("inviz_mts2").style.display="block"
+    document.getElementById("inviz_mts2").style.display="block";
     } else {
-    document.getElementById("inviz_mts2").style.display="none"
+    document.getElementById("inviz_mts2").style.display="none";
  }
-}
-
-function check_fl() {
-  let checked_box = document.getElementById("inviz_fl");
-    if (checked_box.checked) {
-    document.getElementById("inviz_mts2_fl").style.display="block"
-    } else {
-    document.getElementById("inviz_mts2_fl").style.display="none"
-    }
 }
